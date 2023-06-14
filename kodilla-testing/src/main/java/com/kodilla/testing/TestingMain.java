5acd30e07024bf43bd48a22705373ed58949be84
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-14 15:12:31 +0200

    6.2 Added task

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-        System.out.println(onlyVowels("Moduł 6. Wprowadzenie to testowania oprogramowania"));
+//        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+//        System.out.println(onlyVowels("Moduł 6. Wprowadzenie to testowania oprogramowania"));
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("ok");
+        } else {
+            System.out.println("error");
+        }
+
+        Calculator calculator = new Calculator();
+
+        Integer calculatorResult = calculator.add(3, 6);
+
+        if (calculatorResult.equals(9)) {
+            System.out.println("ok");
+        } else {
+            System.out.println("error");
+        }
+
+        calculatorResult = calculator.subtract(7, 2);
+
+        if (calculatorResult.equals(5)) {
+            System.out.println("ok");
+        } else {
+            System.out.println("error");
+        }
 
     }
 

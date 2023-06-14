50d9f515fc42829b4c6b10a5231c4bea5c99a4c2
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-14 14:42:38 +0200

    6.1 Added task

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,5 +3,20 @@ package com.kodilla.testing;
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println(onlyVowels("Moduł 6. Wprowadzenie to testowania oprogramowania"));
+
+    }
+
+    static String onlyVowels(String text) {
+        String resultText = "";
+        String vowels = "aeiou";
+        for (Character character: text.toLowerCase().toCharArray()) {
+            for (Character vowel: vowels.toCharArray()) {
+                if (character.equals(vowel)) {
+                    resultText += vowel;
+                }
+            }
+        }
+        return resultText;
     }
 }

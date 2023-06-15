cae542971c42bbc8ce606a4554b91b01e9f10ac3
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-15 11:30:41 +0200

    6.3 Added method implementation

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class OddNumbersExterminator {
 
     public List<Integer> exterminate(List<Integer> numbers) {
-        return new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (Integer number :
+                numbers) {
+            if (number % 2 == 0) {
+                list.add(number);
+            }
+        }
+        return list;
     }
 
 }

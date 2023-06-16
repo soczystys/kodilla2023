76c9cd999366cbf2ac4ae37ef30a0355d9211bd6
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-16 12:16:53 +0200

    6.4 Added task empty classes

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollector {
+
+    void addFigure(Shape shape) {
+
+    }
+
+    void removeFigure(Shape shape) {
+
+    }
+
+    Shape getFigure(int n) {
+        return new Circle();
+    }
+
+    void showFigures() {
+
+    }
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,5 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollectorTestSuite {
+
+}

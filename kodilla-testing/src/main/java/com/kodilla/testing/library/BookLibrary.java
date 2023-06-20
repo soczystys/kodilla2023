ec26f33bce20f9c9038a445779968e0b3ec75cb8
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-20 11:14:19 +0200

    6.6 Added listBooksInHandsOf

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -21,7 +21,7 @@ public class BookLibrary {
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
-        List<Book> bookList = new ArrayList<>();
+        List<Book> bookList = libraryDatabase.listBooksInHandsOf(libraryUser);
         return bookList;
     }
 

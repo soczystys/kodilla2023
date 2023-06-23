cfcce0b74c881ce764f94a31830f058785e83a0d
kmalinowski957 <kmalinowski957@gmail.com>
2023-06-23 16:54:49 +0200

    8.2 Added additional task

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -8,15 +8,33 @@ public class FirstChallenge {
         return a / b;
     }
 
+    public int pow(int a, int b) {
+//        a ^ b
+        if (a == 0) {
+            return 0;
+        } else if (b == 0) {
+            return 1;
+        } else if (b == 1) {
+            return a;
+        } else {
+            int result = 1;
+            for (int i = 0; i < b; i++) {
+                result *= a;
+            }
+            return result;
+        }
+    }
+
     /**
      * This main can throw an ArithmeticException!!!
      * @param args
      */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-//        double result;
+////        double result;
         try {
-            Double result = firstChallenge.divide(3, 0);
+            int result = firstChallenge.pow(3, 0);
+//            Double result = firstChallenge.divide(3, 0);
             System.out.println(result);
         } catch (Exception e) {
             System.out.println(e);

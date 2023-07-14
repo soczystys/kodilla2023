da7af1f9035af97f75b64c4828ea543e6e698f7d
kmalinowski957 <kmalinowski957@gmail.com>
2023-07-14 11:48:02 +0200

    15.1 Added code before singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    @Test
+    void testGetLastLog() {
+        String stringToLog = "test log";
+        Logger logger = new Logger();
+        logger.log(stringToLog);
+
+        String result = logger.getLastLog();
+
+        assertEquals(stringToLog, result);
+
+    }
+}
